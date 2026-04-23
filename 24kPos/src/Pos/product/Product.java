f0b01e9f6212d795d359a42e73bfdeb4d8fa4aca
0-x-14 <0_x_14@naver.com>
2026-04-23 15:12:48 +0900

    ♻️ refactor: Sale 생성자 수정 및 totalPrice 자동 계산 메서드 추가

--- 24kPos/src/Pos/product/Product.java
+++ 24kPos/src/Pos/product/Product.java
@@ -1,5 +1,34 @@
 package Pos.product;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Product {
+	private long  productId;
+	public String productName;
+	public BigDecimal productPrice;
+	public LocalDateTime expiredAt;
+	private int quantity;
+	
+	
+	public Product(long productId, String productName, BigDecimal productPrice, LocalDateTime expiredAt) {
+	    this.productId = productId;
+	    this.productName = productName;
+	    this.productPrice = productPrice;
+	    this.expiredAt = expiredAt;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public boolean isExpired() {
+		return this.expiredAt.isBefore(LocalDateTime.now());
+	}
+	
 
 }
