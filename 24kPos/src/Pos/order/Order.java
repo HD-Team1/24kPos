31daa9ede5b7fd50a16cce3ba2b7d0f15ce3cac8
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-04-23 13:58:59 +0900

    ✨ Feat: Order 생성자에서 orderId 생성

--- 24kPos/src/Pos/order/Order.java
+++ 24kPos/src/Pos/order/Order.java
@@ -7,7 +7,7 @@ import Pos.product.Product;
 import java.time.LocalDateTime;
 
 public class Order {
-	private int orderId;
+	public long orderId;
 	private Product[] products;
 	private LocalDateTime orderedAt;
 	public enum orderStatus {
@@ -16,6 +16,15 @@ public class Order {
 	private orderStatus status;
 	
 	public Order(Product[] products, orderStatus status) {
+		this.orderedAt = LocalDateTime.now();
+		this.orderId = Integer.parseInt(String.format("%02d%02d%03d", 
+					this.orderedAt.getMonthValue(),
+					this.orderedAt.getDayOfMonth(),
+					Math.abs((Integer.toString(this.orderedAt.getHour())
+					+ Integer.toString(this.orderedAt.getMinute())
+					+ Integer.toString(this.orderedAt.getSecond())
+					+ Integer.toString(this.orderedAt.getNano())).hashCode()%100000)
+				));
 		this.products = products;
 		this.status = status;
 	}
