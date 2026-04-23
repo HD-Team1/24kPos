2943156c9b22700d83d09be2c6f0a36cba0c36b6
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2026-04-23 12:16:52 +0900

    ✏️ Fix: 충돌 해결

--- 24kPos/src/Pos/main/PosMain.java
+++ 24kPos/src/Pos/main/PosMain.java
@@ -1,10 +1,38 @@
 package Pos.main;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import Pos.order.Order;
+import Pos.product.Product;
+import Pos.sale.Sale;
+
 public class PosMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // 필드 선언
+    private String pwd;
+    public Product[] products;
+    private Sale[] history;
+    private BigDecimal dailySalesAmount;
+    private Order[] orders;
 
+    // 발주 목록 조회
+    public Order[] getOrders() {
+        return null;
+    }
+    
+    // 발주 생성
+    public Order makeOrder(Product[] products) {
+        return null;
+    }
+
+    // 발주 취소
+    public boolean cancelOrder(Order order) {
+        return false;
+    }
+    
+    public static void main(String[] args) {
+    	System.out.println("main");
+		
 	}
-
 }
