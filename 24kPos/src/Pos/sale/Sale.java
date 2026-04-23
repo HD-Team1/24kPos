c14d2f3d3889c5f7d99735439070cd18e1bacd6e
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-04-23 12:12:52 +0900

    ✨ Feat: Sale 클래스 구현

--- 24kPos/src/Pos/sale/Sale.java
+++ 24kPos/src/Pos/sale/Sale.java
@@ -1,5 +1,28 @@
 package Pos.sale;
 
-public class Sale {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+import Pos.product.Product;
+
+public class Sale {
+	private int saleId;
+	public Product[] products;
+	public LocalDateTime soldAt;
+	private BigDecimal totalPrice;
+	public enum saleStatus {
+		COMPLETED, CANCELED
+	}
+	private saleStatus status;
+	
+	public Sale(Product[] products, saleStatus status) {
+		this.products = products;
+		this.status = status;
+	}
+	public saleStatus getStatus() {
+		return this.status;
+	}
+	public void setStatus(saleStatus status) {
+		this.status = status;
+	}
 }
